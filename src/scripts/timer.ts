9342e9f026f060e7d69b45d1cb131d98e2e3cb17
Test <damien.trala@outlook.com>
2024-01-02 17:28:33 +0100

    included timer display and create timer class

---
+++ src/scripts/timer.ts
@@ -0,0 +1,29 @@
+export class Timer {
+    private startingTime: number = 300;
+    private remainingTime: number = this.startingTime;
+    private tickCall: NodeJS.Timeout | null = null;
+
+    constructor() {
+        this.tick = this.tick.bind(this); // set "this" context for setInterval
+    }
+
+    private tick() {
+        this.remainingTime--;
+        if (this.remainingTime <= 0) {
+            this.stop();
+        }
+    }
+
+    public start() {
+        this.tickCall = setInterval(this.tick, 1) as NodeJS.Timeout;
+    }
+
+    public stop() {
+        if (this.tickCall !== null) {
+            clearInterval(this.tickCall);
+        }
+    }
+}
+
+let timer = new Timer();
+timer.start();
