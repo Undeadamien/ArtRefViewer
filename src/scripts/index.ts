47f42e84c488d2f97e166cb56e7bb2f4be958540
Test <damien.trala@outlook.com>
2024-01-25 21:20:21 +0100

    extracted api related stuff removed unused stuff

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,63 +1,59 @@
-import { Card, CardList } from "./card";
+import { Card } from "./card";
+import { CardList } from "./cardList";
+import { Form } from "./form";
+import { Timer } from "./timer";
+import { Api } from "./api";
 
-const apiUrl: string = "https://api.scryfall.com/cards/random";
 const prevBut = document.getElementById("previousButton") as HTMLButtonElement;
 const nextBut = document.getElementById("nextButton") as HTMLButtonElement;
 const imageDis = document.getElementById("refDisplay") as HTMLImageElement;
 const nameDis = document.getElementById("nameDisplay") as HTMLParagraphElement;
-const startBut = document.getElementById("switchButton") as HTMLButtonElement;
+const formDis = document.getElementById("form") as HTMLFormElement;
 const overlayDis = document.getElementById("overlayDisplay") as HTMLDivElement;
 const stopBut = document.getElementById("stopButton") as HTMLButtonElement;
-const imageList = new CardList();
 
-async function requestApi(url: string): Promise<Card> {
-    return await fetch(url).then((response) => response.json());
+async function getCardList(amount: number, query: string): Promise<Card[]> {
+    let promises = Array.from({ length: amount }, () => Api.request(query));
+    return Promise.all(promises);
 }
 
-async function getRequestUrl(): Promise<string> {
-    const args: string = ""; // placeholder for future implementation
-    return args ? `${apiUrl}/?q=${args}` : apiUrl;
+function getFormValues(): Form {
+    const formData = new FormData(formDis);
+    return {
+        query: formData.get("query") as string,
+        number: parseInt(formData.get("number") as string),
+        duration: parseInt(formData.get("duration") as string),
+    };
 }
-
-async function getPreviousImage(): Promise<void> {
-    let card = imageList.previous();
-    if (card) {
-        await updateImage(card);
+function handleFormSubmit(event: Event) {
+    event.preventDefault();
+    let formValues = getFormValues();
+    if (isNaN(formValues.number)) {
+        alert("The number of image is invalid");
+        return;
     }
-}
-
-async function getNextImage(): Promise<void> {
-    let card = imageList.next();
-    if (card) {
-        await updateImage(card);
-    } else {
-        let nextImage = await getRequestUrl().then((url) => requestApi(url));
-        imageList.add(nextImage);
-        await updateImage(imageList.next()!); //probably a bad idea
+    if (isNaN(formValues.duration)) {
+        alert("The duration is invalid");
+        return;
     }
+    startSession();
 }
 
-async function updateImage(card: Card): Promise<void> {
-    if (card && card.image_uris && card.image_uris.art_crop) {
-        imageDis.src = card.image_uris.art_crop;
-        nameDis.innerText = card.name;
-    }
-}
+async function startSession() {
+    let formValues = getFormValues();
+    let cardList: CardList = new CardList();
+    cardList.setCards(await getCardList(formValues.number, formValues.query));
 
-function startSession() {
+    // fadeIn overlay
     overlayDis.style.animation = "fadeInOut 0.5s ease-in-out";
     overlayDis.style.visibility = "visible";
 }
 
 function stopSession() {
+    // fadeOut overlay
     overlayDis.style.animation = "fadeOutIn 0.5s ease-in-out";
     setTimeout(() => (overlayDis.style.visibility = "hidden"), 450);
 }
 
-prevBut.onclick = () => getPreviousImage();
-nextBut.onclick = () => getNextImage();
-stopBut.onclick = () => stopSession();
-startBut.addEventListener("click", (event) => {
-    event.preventDefault();
-    startSession();
-});
+formDis.addEventListener("submit", handleFormSubmit);
+stopBut.addEventListener("click", stopSession);
