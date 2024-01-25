0a03fadde27fdf55dabd951056376819e6e4ef7e
Test <damien.trala@outlook.com>
2024-01-25 21:18:03 +0100

    moved cardList out of card.ts

--- src/scripts/card.ts
+++ src/scripts/card.ts
@@ -3,30 +3,3 @@ export interface Card {
     image_uris?: { art_crop?: string }; // may be undefined for two sided cards
     uri?: string[]; // need to check
 }
-
-export class CardList {
-    private cards: Card[];
-    private index: number;
-    constructor() {
-        this.index = -1;
-        this.cards = [];
-    }
-    current(): Card {
-        return this.cards[this.index];
-    }
-    previous(): Card | undefined {
-        if (this.index > 0) {
-            this.index--;
-            return this.cards[this.index];
-        }
-    }
-    next(): Card | undefined {
-        if (this.index < this.cards.length - 1) {
-            this.index++;
-            return this.cards[this.index];
-        }
-    }
-    add(card: Card) {
-        this.cards.push(card);
-    }
-}
